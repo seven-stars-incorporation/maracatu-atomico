e407c5e90026a501532e2f2ef9b35ef2e20be743
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-09-27 15:07:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,74 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
-# import os
+from noticias import Noticias
 
-# G1
-def g1():
-    url = 'http://g1.com.br/'
-    header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                    'AppleWebKit/537.36 (KHTML, like Gecko) '
-                    'Chrome/51.0.2704.103 Safari/537.36'}
+print("Teste para POO\nNoticias do g1")
+Noticias.g1()
 
-    resposta = requests.get(url, headers= header)
-
-    if resposta.status_code == 200:
-        conteudo = resposta.content
-        site = BeautifulSoup(conteudo, 'html.parser')
-        noticia = site.find('div', attrs={'class' : 'row medium-uncollapse bastian-container small-collapse medium-collapse large-uncollapse'})
-
-        lista_links = []
-
-        for link in noticia.find_all('a'):
-            lista_links.append(link.get('href'))
-
-        # Esses indices se repitiram em todos os testes, por isso estou tirando eles
-        del lista_links[1]
-        del lista_links[1]
-        del lista_links[2]
-        del lista_links[3]
-        del lista_links[3]
-
-        for i in range(0, 5):
-            print(f'Link: {lista_links[i]}')
-
-    else:
-        print(f'Minha requisição foi negada :disappointed:\nStatus: {resposta.status_code}')
-
-
-# Estadão
-def estadao():
-    url = 'https://www.estadao.com.br/'
-    header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                            'AppleWebKit/537.36 (KHTML, like Gecko) '
-                            'Chrome/51.0.2704.103 Safari/537.36'}
-
-    resposta = requests.get(url, headers= header)
-
-    if resposta.status_code == 200:
-
-        conteudo = resposta.content
-        site = BeautifulSoup(conteudo, 'html.parser')
-
-        noticia = site.find('div', attrs={'class' : 'intro'})
-
-        lista_links = []
-
-        for link in noticia.find_all('a'):
-            lista_links.append(link.get('href'))
-
-        for i in range(0, 3):
-            print(f'Link: {lista_links[i]}')
-
-    else:
-        print(f'Minha requisição foi negada :disappointed:\nStatus: {resposta.status_code}')
-
-#Em Desenvolvimento:
-
-# R7
-def r7():
-    print(f'Esse site tá perguntando se eu sou um robô :disappointed:\nAguenta um pouquinho que eu vou enganar ele hehehe :smiling_imp:\nCaso queria acessar, está aqui o link: https://www.r7.com/')
-
-# Google Noticias
-def gn():
-    print(f'Esse site tá perguntando se eu sou um robô :disappointed:\nAguenta um pouquinho que eu vou enganar ele hehehe :smiling_imp:\nCaso queria acessar, está aqui o link: https://news.google.com/')
-
+print("\nNoticias do Estadão:")
+Noticias.estadao()
