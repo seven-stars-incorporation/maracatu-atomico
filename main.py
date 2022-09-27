ead4d1e6577c7c993241128c8daee2a66531167e
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-09-27 14:44:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,108 +1,74 @@
-import discord, requests
+import requests
 from bs4 import BeautifulSoup
 # import os
 
-client = discord.Client()
+# G1
+def g1():
+    url = 'http://g1.com.br/'
+    header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                    'AppleWebKit/537.36 (KHTML, like Gecko) '
+                    'Chrome/51.0.2704.103 Safari/537.36'}
 
-@client.event
-async def on_ready():
-    print('Tudo certo. Bot: {0.user}'.format(client))
+    resposta = requests.get(url, headers= header)
 
+    if resposta.status_code == 200:
+        conteudo = resposta.content
+        site = BeautifulSoup(conteudo, 'html.parser')
+        noticia = site.find('div', attrs={'class' : 'row medium-uncollapse bastian-container small-collapse medium-collapse large-uncollapse'})
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+        lista_links = []
 
-    if message.content.startswith('$help'):
-        await message.channel.send(
-            'Olá, sou o bot de nóticias :smiling_face_with_3_hearts: ```\n$help => Mostra os comandos\n$ping => Mostra o ping da minha casa até aqui\n\n$g1 => 5 Noticias do G1\n$estadao => 3 Noticias do Estadão\n\n>>>>>COMANDOS EM DESESNVOLVIMENTO<<<<\n$r7 => Noticias da Record\n$gn => Google News```'
-            )
+        for link in noticia.find_all('a'):
+            lista_links.append(link.get('href'))
 
-    if message.content.startswith('$ping'):
-        await message.channel.send(
-            f'Pong! :ping_pong: {round(client.latency * 1000)}ms'
-        )
+        # Esses indices se repitiram em todos os testes, por isso estou tirando eles
+        del lista_links[1]
+        del lista_links[1]
+        del lista_links[2]
+        del lista_links[3]
+        del lista_links[3]
 
-# A brincadeira começa daqui :)
+        for i in range(0, 5):
+            print(f'Link: {lista_links[i]}')
 
-    # G1
-    if message.content.startswith('$g1'):
-    
-        url = 'http://g1.com.br/'
-        header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                        'AppleWebKit/537.36 (KHTML, like Gecko) '
-                        'Chrome/51.0.2704.103 Safari/537.36'}
-
-        resposta = requests.get(url, headers= header)
-
-        if resposta.status_code == 200:
-
-            conteudo = resposta.content
-            site = BeautifulSoup(conteudo, 'html.parser')
-            noticia = site.find('div', attrs={'class' : 'row medium-uncollapse bastian-container small-collapse medium-collapse large-uncollapse'})
-
-            lista_links = []
-
-            for link in noticia.find_all('a'):
-                lista_links.append(link.get('href'))
-
-            # Esses indices se repitiram em todos os testes, por isso estou tirando eles
-            del lista_links[1]
-            del lista_links[1]
-            del lista_links[2]
-            del lista_links[3]
-            del lista_links[3]
-
-            for i in range(0, 5):
-                await message.channel.send(f'Link: {lista_links[i]}')
-        else:
-            await message.channel.send(
-                f'Minha requisição foi negada :disappointed:\nStatus: {resposta.status_code}'
-                )
+    else:
+        print(f'Minha requisição foi negada :disappointed:\nStatus: {resposta.status_code}')
 
 
-    # Estadão
-    if message.content.startswith('$es'):
+# Estadão
+def estadao():
+    url = 'https://www.estadao.com.br/'
+    header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                            'AppleWebKit/537.36 (KHTML, like Gecko) '
+                            'Chrome/51.0.2704.103 Safari/537.36'}
 
-        url = 'https://www.estadao.com.br/'
-        header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                                'AppleWebKit/537.36 (KHTML, like Gecko) '
-                                'Chrome/51.0.2704.103 Safari/537.36'}
+    resposta = requests.get(url, headers= header)
 
-        resposta = requests.get(url, headers= header)
+    if resposta.status_code == 200:
 
-        if resposta.status_code == 200:
+        conteudo = resposta.content
+        site = BeautifulSoup(conteudo, 'html.parser')
 
-            conteudo = resposta.content
-            site = BeautifulSoup(conteudo, 'html.parser')
+        noticia = site.find('div', attrs={'class' : 'intro'})
 
-            noticia = site.find('div', attrs={'class' : 'intro'})
+        lista_links = []
 
-            lista_links = []
+        for link in noticia.find_all('a'):
+            lista_links.append(link.get('href'))
 
-            for link in noticia.find_all('a'):
-                lista_links.append(link.get('href'))
+        for i in range(0, 3):
+            print(f'Link: {lista_links[i]}')
 
-            for i in range(0, 3):
-                await message.channel.send(f'Link: {lista_links[i]}')
-
-        else:
-            await message.channel.send(
-                f'Minha requisição foi negada :disappointed:\nStatus: {resposta.status_code}'
-                )
+    else:
+        print(f'Minha requisição foi negada :disappointed:\nStatus: {resposta.status_code}')
 
 #Em Desenvolvimento:
 
-    # R7
-    if message.content.startswith('$r7'):
-        await message.channel.send(f'Esse site tá perguntando se eu sou um robô :disappointed:\nAguenta um pouquinho que eu vou enganar ele hehehe :smiling_imp:\nCaso queria acessar, está aqui o link: https://www.r7.com/')
+# R7
+def r7():
+    print(f'Esse site tá perguntando se eu sou um robô :disappointed:\nAguenta um pouquinho que eu vou enganar ele hehehe :smiling_imp:\nCaso queria acessar, está aqui o link: https://www.r7.com/')
 
-    # Google Noticias
-    if message.content.startswith('$gn'):
-        await message.channel.send(f'Esse site tá perguntando se eu sou um robô :disappointed:\nAguenta um pouquinho que eu vou enganar ele hehehe :smiling_imp:\nCaso queria acessar, está aqui o link: https://news.google.com/')
+# Google Noticias
+def gn():
+    print(f'Esse site tá perguntando se eu sou um robô :disappointed:\nAguenta um pouquinho que eu vou enganar ele hehehe :smiling_imp:\nCaso queria acessar, está aqui o link: https://news.google.com/')
 
-
-
-
-client.run('OTI4NzQ4MjY2NzE0MTc3NjA3.YddSOg.dAVVfKUQa-WmrBFlh8jHdXt6D9E')
