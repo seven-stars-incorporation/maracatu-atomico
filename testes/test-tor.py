fc549818d25793abc438c5934e18f728b8d8dc10
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-09-27 16:07:12 -0300

    Create test-tor.py

---
+++ testes/test-tor.py
@@ -0,0 +1,11 @@
+import requesocks 
+req = requesocks.session() 
+req.proxies = {'http': 'socks5://127.0.0.1:9050', 
+    'https': 'socks5://127.0.0.1:9050'
+    } 
+    
+def request_tor(url): 
+    page = req.get(url) 
+    text = page.text 
+    return text 
+    print(request_tor)
